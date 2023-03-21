a86ae235bb9341ae854cf9fcf6ab6e7da5692396
hana <han02hana06@gmail.com>
2023-03-21 15:31:22 +0900

    계산 안됨

--- recoil/src/App.js
+++ recoil/src/App.js
@@ -8,61 +8,116 @@ import{
   useRecoilValue
 } from "recoil";
 
-const textState=atom({
-  key:"textState",
+const oButton =[
+  "+",
+  "-",
+  "*",
+  "/",
+];
+
+const num1State =atom({
+  key: "num1State",
   default:""
 });
 
-const charCountState = selector({
-  key:"charCountState",
+const num2State =atom({
+  key: "num2State",
+  default:""
+});
+
+const num1useState= selector({
+  key:"num1useState",
   get:({get})=>{
-    const text = get(textState);
-    return text;
+    const num1= get(num1State);
+
+    return num1;
+  },
+});
+
+const num2useState = selector({
+  key:"num2useState",
+  get: ({get}) =>{
+    const num2 = get(num2State);
+
+    return num2;
+  },
+});
+
+function NumInput(){
+  const [num1, setNum1]= useRecoilState(num1State);
+  const [num2, setNum2] =useRecoilState(num2State);
+  const onChange1=(event)=>{
+    setNum1(event.target.value);
   }
-})
-
-function TextInput(){
-  const [text, setText]=useRecoilState(textState);
-
-  const onChange = (event) =>{
-    setText(event.target.value);
+  const onChange2=(event)=>{
+    setNum2(event.target.value);
   }
 
   return(
-    <div>
-      <input type="text" value={text} onChange={onChange}/>
-      <br />
-      ECHO: {text}
-    </div>
+   <div><h2>
+    Number1 <input type="number" id="num1" value={num1} onChange={onChange1}/><br/>
+    Number2 <input type="number" id="num2" value={num2} onChange={onChange2}/><br/>
+    </h2>
+    <hr/>
+    Echo:{num1}, {num2}
+   </div>
   );
 }
 
-function CharacterCount(){
-  const digit = useRecoilValue(charCountState);
 
-  return <>add 10: { Number.parseInt(digit)+10}</>;
+function Plus(){
+  const plus1 = useRecoilValue(num1useState);
+  const plus2 = useRecoilValue(num2useState);
+  const presult= Number.parseInt(plus1)+Number.parseInt(plus2);
+
+  return presult;
+
 }
 
-function CharacterCounter(){
+function Minus(){
+  const minus1 = useRecoilValue(num1useState);
+  const minus2 = useRecoilValue(num2useState);
+
+  return <>{Number.parseInt(minus1)-Number.parseInt(minus2)}</>;
+}
+
+function Multiply(){
+  const multiply1 = useRecoilValue(num1useState);
+  const multiply2 = useRecoilValue(num2useState);
+
+  return <>{multiply1*multiply2}</>;
+}
+
+function Divide(){
+  const divide1 = useRecoilValue(num1useState);
+  const divide2 = useRecoilValue(num2useState);
+
+  return <>{divide1/divide2}</>;
+}
+
+function Operation(){
   return(
     <div>
-      <TextInput/>
-      <CharacterCount/>
+      <button >+</button><br/>
+      <button>-</button><br/>
+      <button>*</button><br/>
+      <button>/</button>
     </div>
-  );
+  )
 }
 
-function App() {
-  return (
-    <div className="App">
-      <RecoilRoot>
-        <div className='App'>
-          <h1>Hello GCU-Kakao</h1>
-          <h2>ADD TEN</h2>
-          <CharacterCounter />
-        </div>
-      </RecoilRoot>
-    </div>
+function App(){
+
+  return(
+    <RecoilRoot>
+      <div className='App'>
+      <h1>GCU CALCULATOR</h1>
+      <NumInput/>
+      <div> <h3>Operator&nbsp;&nbsp;&nbsp;Result</h3>
+      <Operation/>
+      </div>
+      </div>
+    </RecoilRoot>
   );
 }
 
